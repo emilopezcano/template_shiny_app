39a1f5b40b48d3c9aa79dbee6b2cfe0c5dbd841c
emilopezcano <emilopezcano@gmail.com>
2024-06-10 16:17:35 +0200

    theme tests

--- R/app_server.R
+++ R/app_server.R
@@ -1,5 +1,7 @@
 
 app_server <- function(input, output) {
+  # Uncomment to include a theme selector
+  # bs_themer() 
   output$p <- renderPlot({
     ggplot(penguins) +
       geom_histogram(aes(!!input$var), bins = input$bins) +
--- R/app_ui.R
+++ R/app_ui.R
@@ -2,6 +2,7 @@ app_ui <- function(request){
   page_sidebar(
     window_title = "dash",
     title = "My dashboard",
+    theme = mytheme(),
     sidebar = "Sidebar",
     "Main content area"
   )
