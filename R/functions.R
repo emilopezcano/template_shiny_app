8d2fa7bd1d473de2aa1a756c4b3b9df70062bc75
emilopezcano <emilopezcano@gmail.com>
2024-06-10 16:17:01 +0200

    functions ui

--- R/functions.R
+++
@@ -1,4 +0,0 @@
-## Include functions for:
-## - Generate reactives from inputs
-## - Generate outputs from inputs
-## - Generate inputs, e.g., from config
---
+++ R/functions_ui.R
@@ -0,0 +1,22 @@
+## Include functions for:
+## - Generate reactives from inputs
+## - Generate outputs from inputs
+## - Generate inputs, e.g., from config
+
+
+mytheme <- function(){
+  bs_theme(
+    bg = "#FFF",
+    fg = "#002659",
+    primary = "#fab117",
+    secondary = "#0072B2",
+    success = "#5cae6e",
+    danger = "#d96363",
+    base_font = font_google("Inter"),
+    code_font = font_google("JetBrains Mono")
+  )
+}
+
+mymenu <- function(){
+  
+}
