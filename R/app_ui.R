b1cbab5eb5e4206d7dd05aa5c5f05908b85a314c
emilopezcano <emilopezcano@gmail.com>
2024-06-11 00:24:24 +0200

    value_boxes

--- R/app_ui.R
+++ R/app_ui.R
@@ -1,9 +1,26 @@
 app_ui <- function(request){
-  page_sidebar(
+  page_navbar(
     window_title = "dash",
     title = "My dashboard",
     theme = mytheme(),
-    sidebar = "Sidebar",
-    "Main content area"
+    inverse = TRUE,
+    sidebar = sidebar(title = "Selections", 
+                      position = "left"),
+    nav_panel(title = "Overview",
+              layout_sidebar(sidebar = sidebar(position = "right",
+                                               myvbs()[1],
+                                               myvbs()[2],
+                                               myvbs()[3]),
+                             layout_columns(fill = FALSE,
+                                            col_widths = c(6,6),
+                                            card("map"),
+                                            card("plot")
+                             )
+              ),
+    ),
+    nav_panel(title = "Section 1",
+              "Content of second section",
+              layout_columns(myvbs()[1], myvbs()[2], myvbs()[3]))
+    
   )
 }
--- R/functions_ui.R
+++ R/functions_ui.R
@@ -7,9 +7,11 @@
 mytheme <- function(){
   bs_theme(
     bg = "#FFF",
-    fg = "#002659",
+    fg = "#001F66",
+    # "navbar-fg" = "#FFF",
+    # "navbar-bg" = "#001F66",
     primary = "#fab117",
-    secondary = "#0072B2",
+    secondary = "#e1ebee",
     success = "#5cae6e",
     danger = "#d96363",
     base_font = font_google("Inter"),
@@ -19,4 +21,38 @@ mytheme <- function(){
 
 mymenu <- function(){
   
+}
+
+myvbs <- function(values = 1:3, 
+                  icons = c("graph-up", "thermometer-sun", "handbag"),
+                  pos = "top right",
+                  .fill = FALSE,
+                  .size = "0.5em"){
+  list(value_box(
+    title = "Correlation",
+    value = values[1],
+    theme = "warning",
+    showcase = bsicons::bs_icon(icons[1],
+                                size = .size),
+    showcase_layout = pos,
+    fill = .fill
+  ),
+  value_box(
+    title = "KPI_2",
+    value = values[2],
+    theme = "danger",
+    showcase = bsicons::bs_icon(icons[2],
+                                size = .size),
+    showcase_layout = pos,
+    fill = .fill
+  ),
+  value_box(
+    title = "KPI_3",
+    value = values[3],
+    theme = "success",
+    showcase = bsicons::bs_icon(icons[3],
+                                size = .size),
+    showcase_layout = pos,
+    fill = .fill
+  ))
 }
